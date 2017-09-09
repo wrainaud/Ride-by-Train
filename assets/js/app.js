f1f2e3475db77d5ddcafcf03d28b8b7e92acfa83
William Rainaud <wrainaud@gmail.com>
2017-09-09 18:21:05 -0400

    Add Foundation / CSS

---
+++ assets/js/app.js
@@ -0,0 +1,39 @@
+// closes the panel on click outside
+$(document).mouseup(function (e) {
+  var container = $('#contact-panel');
+  if (!container.is(e.target) // if the target of the click isn't the container...
+  && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+      container.removeClass('is-active');
+    }
+});
+
+// Assumptions
+    var tFrequency = 3;
+
+    // Time is 3:30 AM
+    var firstTime = "03:30";
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % tFrequency;
+    console.log(tRemainder);
+
+    // Minute Until Train
+    var tMinutesTillTrain = tFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
