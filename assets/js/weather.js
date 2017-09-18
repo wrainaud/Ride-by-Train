aea490ff222df26c7a3959c617b89b53b0b3a8a7
William Rainaud <wrainaud@gmail.com>
2017-09-18 17:37:11 -0400

    Add Google Maps / Weather Icon, Time Styles

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -6,6 +6,7 @@
 // Declare Variables
 var lat;
 var lon;
+var map;
 
 // Document Ready
 $(document).ready(function(){
@@ -14,9 +15,10 @@ $(document).ready(function(){
 
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
-      var lat = "lat=" + position.coords.latitude;
-      var lon = "lon=" + position.coords.longitude;
+      var lat = position.coords.latitude;
+      var lon = position.coords.longitude;
       getWeather(lat, lon);
+      initializeMap(lat, lon);
     });
   } else {
     console.log("Geolocation is not supported by this browser.");
@@ -28,7 +30,8 @@ function getWeather(lat, lon) {
 
 	// var darkSkyAPI = "https://api.darksky.net/forecast/c302a50d97629336f69dbb53384f75fb/" + lat + "," + lon;
 
-	var owAPIurl = "http://api.openweathermap.org/data/2.5/forecast?" + lat + "&" + lon + "&units=imperial&APPID=4e0ae26b68029fdb4141d49a573c0b8f";
+	var owAPIurl = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=imperial&APPID=4e0ae26b68029fdb4141d49a573c0b8f";
+  // var newIcon = result.list[0].weather[0].icon + ".png";
 
 	$.ajax({
     url: owAPIurl,
@@ -37,14 +40,38 @@ function getWeather(lat, lon) {
 	})
 
 	.done(function(result) {
+      console.log(result);
+
       $("#location").text(result.city.name + ", " + result.city.country);
       $("#desc").html("Current Forecast: " + titleCase(result.list[0].weather[0].description));
-      $("#current-temp").text("Current Temperature: " + result.list[0].main.temp + "°");
-      $("#high-temp").text("High: " + result.list[0].main.temp_max + "°");
-      $("#low-temp").text("Low: " + result.list[0].main.temp_min + "°");
+      $("#icon").html('<img src=' + "http://openweathermap.org/img/w/" + result.list[0].weather[0].icon + ".png" + ">");
+      $("#current-temp").html("Current Temperature: " + result.list[0].main.temp + ' <i class="wi wi-fahrenheit"></i>');
+      $("#high-temp").html("High: " + result.list[0].main.temp_max + ' <i class="wi wi-fahrenheit"></i>');
+      $("#low-temp").html("Low: " + result.list[0].main.temp_min + ' <i class="wi wi-fahrenheit"></i>');
     
     })
 };
+  
+  
+// setup initial map
+function initializeMap(lat, lon) {
+
+    var myLocation = {lat: lat, lng: lon};
+   
+    var map = new google.maps.Map(document.getElementById('map-canvas'), {
+    mapTypeId: 'satellite',  
+    zoom: 10,
+    center: myLocation
+    // mapTypeId:'roadmap'
+    });
+
+    var marker = new google.maps.Marker({
+    position: myLocation,
+    map: map
+    });
+}
+    
+  
 
 // Function to Title Case Weather Description
 
@@ -59,5 +86,6 @@ function titleCase(str) {
 
     return str.join(' '); 
     };
+
 
 });
